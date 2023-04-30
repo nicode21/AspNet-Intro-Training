0e6bff2b4dc85df46b8cbd2864201da343bca935
nicode21 <nijatan@code.edu.az>
2023-04-30 04:48:09 +0400

    add files

---
+++ AspNet-intro/AspNet-intro/AspNet-intro/Controllers/AboutController.cs
@@ -0,0 +1,10 @@
+﻿namespace AspNet_intro.Controllers
+{
+    public class AboutController
+    {
+        public string Index()
+        {
+            return "This is a about page";
+        }
+    }
+}
---
+++ AspNet-intro/AspNet-intro/AspNet-intro/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet_intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            int[] nums = {1, 2, 3, 4, 5, 6, 7, 8};
+            return View(nums) /*Json(new { name = "Resul",surname = "Hesenov"})*/ /*Ok("Success")*/ /*NotFound()*/ /*BadRequest()*/;
+        }
+
+
+      /*  public string Index(*//*int num, string num2*/ /*int id*//*)
+        {
+            return "This is a home page"*//*+ num + " - " + num2*//*;
+        }
+
+        public string Test(int id)
+        { 
+            return "This is a test action --- " + id;
+        }*/
+    }
+}
